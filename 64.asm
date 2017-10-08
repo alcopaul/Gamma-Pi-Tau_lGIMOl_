9aa1a6a30d83524f7a7ef45d579dc1e82100b25b
alcopaul <paul.amurao@gmail.com>
2017-10-08 10:20:19 -0400

    Create 64.asm

---
+++ 64.asm
@@ -0,0 +1,20 @@
+extrn MessageBoxA: PROC
+extrn ExitProcess: PROC
+
+.data
+header db 'by SATAN', 0
+msg db 'Everyday Le Club [As]', 0
+
+.code
+main proc
+     sub rsp, 28h        ; 8x5 
+     mov rcx, 0
+     lea rdx, msg
+     lea r8,  header
+     mov r9d, 0
+     call MessageBoxA
+     add rsp, 28h
+     mov ecx, eax
+     call ExitProcess
+main endp
+end
