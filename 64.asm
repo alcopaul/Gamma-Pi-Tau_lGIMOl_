25aa08795f389b809741494f9f7dc270351947bf
alcopaul <paul.amurao@gmail.com>
2018-01-23 10:53:19 -0500

    Update 64.asm

--- 64.asm
+++ 64.asm
@@ -7,14 +7,14 @@ msg db 'Everyday Le Club [As]', 0
 
 .code
 main proc
-     sub rsp, 28h
+     sub rsp, 28h        ; 8x5 
      mov rcx, 0
      lea rdx, msg
      lea r8,  header
      mov r9d, 0
      call MessageBoxA
      add rsp, 28h
-     mov ecx, eax
+     mov rcx, rax
      call ExitProcess
 main endp
 end
