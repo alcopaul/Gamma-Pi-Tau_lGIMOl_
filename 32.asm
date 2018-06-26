a839bf457b949bd0ba0fc36be8f24f153af47f6c
a l C o P a U L <paul.amurao@gmail.com>
2018-06-26 11:28:21 -0400

    Update 32.asm

--- 32.asm
+++ 32.asm
@@ -1,20 +1,22 @@
-.386		
-.Model Flat,StdCall
+.386
+.model flat, stdcall
 
-extrn   ExitProcess: PROC     
-extrn   MessageBoxA: PROC
+ExitProcess PROTO, uExitCode:DWORD
+MessageBoxA PROTO, hWnd:DWORD, lpText:DWORD, lpCaption:DWORD, uType:DWORD
 
-.Data                                        
-text    db "Everyday Le Club [As]", 0                                
-caption db "by SALPSAN", 0
+.data
+text 	db 'EVERYDAY Le Club [As]', 0
+caption db 'by SALPSAN', 0
 
-.Code                                  
-Main:				
-        push 0
-        push offset caption
-        push offset text
-        push 0
-        call MessageBoxA
-        push eax
-        call ExitProcess     
-End Main
+.code
+main:
+	push 0
+	push offset caption
+	push offset text
+	push 0	
+	call MessageBoxA
+	push eax
+	call ExitProcess
+end main
+
+;ml.exe 32.asm /link /subsystem:windows /defaultlib:kernel32.lib /defaultlib:user32.lib /entry:main
