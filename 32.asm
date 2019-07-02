6d8cf955abed905f54216f809f8e8d06fa4bd765
a l C o P a U L <paul.amurao@gmail.com>
2019-07-02 10:39:09 -0400

    Update 32.asm

--- 32.asm
+++ 32.asm
@@ -9,11 +9,11 @@ header  db 'by SALPSAN', 0
 msg 	db 'EVERYDAY Le Club [As]', 0
 
 .code
-start:
-main proc
+main:
+start proc
         invoke MessageBoxA, 0, addr msg, addr header, 0
         invoke ExitProcess, eax
-main endp
-end start
+start endp
+end main
 
-;ml.exe 32.asm /link /subsystem:windows /defaultlib:user32.lib /defaultlib:kernel32.lib /entry:start
+;ml.exe 32.asm /link /subsystem:windows /defaultlib:user32.lib /defaultlib:kernel32.lib /entry:main
