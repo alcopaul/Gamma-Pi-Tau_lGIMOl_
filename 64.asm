274f7fd92e812d1a7b03a0a360e5a2cb1abf3127
a l C o P a U L <paul.amurao@gmail.com>
2019-07-02 11:53:20 -0400

    Update 64.asm

--- 64.asm
+++ 64.asm
@@ -6,8 +6,9 @@ header db 'by SAGSSHAN', 0
 msg db 'EVERYDAY Le Club [As]', 0
 
 .code
+
 main proc
-     sub rsp, 28h 
+     sub rsp, 28h
      mov rcx, 0
      lea rdx, msg
      lea r8,  header
@@ -17,6 +18,6 @@ main proc
      mov rcx, rax
      call ExitProcess
 main endp
-end
+end 
 
 ;ml64.exe 64.asm /link /subsystem:windows /defaultlib:user32.lib /defaultlib:kernel32.lib /entry:main
