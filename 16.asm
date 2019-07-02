1e6e1068f0ef43aaf830a22708df0f94896ae29b
a l C o P a U L <paul.amurao@gmail.com>
2019-07-02 11:27:33 -0400

    Add files via upload

---
+++ 16.asm
@@ -0,0 +1,18 @@
+.model tiny
+.stack
+
+.data
+msg db "-------------------------", 0Dh, 0Ah, "| by [GIMO]             |", 0Dh, 0Ah, "|-----------------------|", 0Dh, 0Ah, "|                       |", 0Dh, 0Ah, "| EVERYDAY Le Club [As] |", 0Dh, 0Ah, "|                       |", 0Dh, 0Ah, "-------------------------", "$"
+
+.code
+start:
+main proc
+     push cs
+     pop ds
+     lea dx, msg
+     mov ah, 09h
+     int 21h
+     mov ax, 4C00h
+     int 21h
+main endp
+end start
